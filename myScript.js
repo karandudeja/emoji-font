07214bf32d6c96551b5eae81a31ebe8ad0b606dd
Karan Dudeja <16996@KDs-MacBook-Pro.local>
2020-08-08 21:52:24 +0530

    new edits and js cleaned and commented

--- myScript.js
+++ myScript.js
@@ -5,13 +5,13 @@ var rules = document.getElementById("disclaimer");
 var headingText = document.getElementById("heading");
 var emojiString = "";
 var newEmojiString = "";
-var removeNumCharsFromBack = 16;
 
 let strobeCount = 0;
 let strobeReps = 8;
-let strobeOneCycleDuration = 0.16; // write as per normal human seconds where 1 sec is 1
+let strobeOneCycleDuration = 0.16; // write as per normal human seconds where 1 sec is 1.0
 document.body.onload = strobeEffect(strobeReps, strobeOneCycleDuration);
 
+//function calls the strobe effect, every time on page load
 function strobeEffect(sr, sd){
     rules.style.visibility = "visible";
     let runStrobe = setInterval(changeBgColor, sd*1000);
@@ -20,26 +20,15 @@ function strobeEffect(sr, sd){
         rules.style.backgroundColor = rules.style.backgroundColor == "black" ? "white" : "black";
         rules.style.color = rules.style.backgroundColor == "white" ? "black" : "white";
         strobeCount++;
-        //console.log(`strobeCount is : ${strobeCount}`);
         if(strobeCount > sr){
             clearInterval(runStrobe);
             rules.style.backgroundColor = "#f6f6f6";
-            rules.style.color =  "#888";
+            rules.style.color =  "#999";
         }
-    }
-    
+    }   
 }
 
-
-
-function bgWhite(){
-    console.log("make BG white");
-    rules.style.backgroundColor = "white";
-    rules.style.color = "black";
-}
-
-
-//new attempt with input box's value, using 'input' in the eventListener
+//take input box's value, using 'input' in the eventListener and create the mapped emoji text-string
 var emoS = "";
 userTxt.addEventListener('input', function(e){
     emoS = "";
@@ -56,7 +45,7 @@ userTxt.addEventListener('input', function(e){
             case " ":
               emoS += "&nbsp;&nbsp;&nbsp;&nbsp;";
               break;
-            //////////// lowercase //////
+            //////////// lowercase characters //////
             case "a":
               emoS += "🔓";
               //console.log("entered a");
@@ -161,7 +150,7 @@ userTxt.addEventListener('input', function(e){
               emoS += "💤";
               //console.log("entered z");
               break;
-            //////////// numbers //////
+            //////////// numbers ///////////////
             case "0":
               emoS += "⚫";
               //console.log("entered z");
@@ -202,10 +191,9 @@ userTxt.addEventListener('input', function(e){
                 emoS += "🕘";
                 //console.log("entered z");
                 break;
-            ////////// special characters ////////
+            ////////// special characters ////////////
             case "?":
                 emoS += "❓";
-                //console.log("entered z");
                 break;
             case ".":
                 emoS += "🚷";
@@ -236,7 +224,7 @@ userTxt.addEventListener('input', function(e){
     emojifiedTxt.innerHTML = emoS;
 }, false);
 
-
+//remove the bottom credits on mobile devices, when input is in focus
 userTxt.addEventListener('focus', () => {
     changeHeadingText();
     if (window.matchMedia("(max-width: 400px)").matches) {
@@ -247,11 +235,12 @@ userTxt.addEventListener('focus', () => {
     }
 });
 
+//show the bottom credits on mobile devices, when input is out of focus(blurred as per CSS terms)
 userTxt.addEventListener('blur', () => {
     credits.style.visibility = 'visible';
 });
 
-
+//change the "o" in the "emoji font" title every time the user clicks the input text box
 function changeHeadingText(){
     let oArray = ["📀","💿","🎡","🔅","😮","😎","🙃","😵","🌚","🌞","🌎","🌕","🍩","🥚","🏀","🎱","⚽","⚙️","🎈","⭕","🚫","🅾️","🚭","🌀","0️⃣","⏺️","🔄","🔘","☢️","☯️","☮️","☸️","🔮","⭐","🐡","🐤","🐧","🐼","🙆‍♂️","👁️","💍","🌼","💥","🍊"];
     let r1 = oArray[Math.floor(Math.random()*oArray.length)];
@@ -260,190 +249,10 @@ function changeHeadingText(){
     headingText.innerHTML = newHeading;
 }
 
-//lowercase characters
+//lowercase characters, only some in use
 /*a🐊🔓 b💪🗽 c🗜️ d🍶 e🍥 f🎋🌾 g🐬🦈 h🐪🐢 i📍 j🏒 k🎿🎬  l🕯️ m⚖️🎹 n⛩️🎵 o📀 p🌵🚏🚩 q📭 r🥀🌈🌱 s💲♨️⚡ t🍌 u⛎👅 v💎🔻w🔱 x✂️🛠️⚒️⚔️ 󠁧󠁢󠁳󠁣󠁴 y🏆🤸‍♂️ z💤*/
 
 //uppercase characters NOT in use
 /*A🗼⛰️ B🍤🅱️ C🌜🌊 D🍤🌛 E🏯 F🎋🎏 G🌀 H I🚹 J🎷K🎿⏮️ L M〽️⚖️ N📉🎢<br/><br/>O🍩 P Q🍭🍳R S T U V W〰️ X✂️🛠️⚒️⚔️ Y Z💤*/
 
 //numbers 0123456789 ⚫🕐🕑🕒🕓🕔🕕🕖🕗🕘
-
-
-
-
-
-// function checkString(str) {
-//   console.log("checkString called...");
-//   if (str.charAt(str.length - 1) === "�") {
-//     console.log("have to remove � !!!");
-//     //newEmojiString = emojiString.slice(0, -1);
-//     //emojiString = newEmojiString;
-//   }
-// }
-
-//as of before 4 Aug 2020, using keyup and keydown
-/*userTxt.addEventListener("keydown", function (event) {
-  var keyEntered = event.code;
-  console.log(keyEntered);
-  //console.log(userTxt.value);
-  //makeEmojiString(userTxt.value);
-  if (userTxt.value.length < 1) {
-    emojiString = "";
-  }
-
-  switch (keyEntered) {
-    case "Space":
-      emojiString += "&nbsp;&nbsp;&nbsp;";
-      break;
-    case "Backspace":
-      newEmojiString = emojiString.slice(0, -2);
-      console.log(`new Emoji String is: ${newEmojiString}`);
-      console.log(
-        "last character newEmojiString is : " +
-          newEmojiString.charAt(newEmojiString.length - 1)
-      );
-      console.log(
-        "type of last char is : " +
-          typeof newEmojiString.charAt(newEmojiString.length - 1)
-      );
-      //handling the � as last character
-      if (newEmojiString.charAt(newEmojiString.length - 1) === "�") {
-        console.log("have to remove � !!!");
-        newEmojiString = emojiString.slice(0, -1);
-        emojiString = newEmojiString;
-      }
-      //handling the extra space removals
-      else if (newEmojiString.charAt(newEmojiString.length - 1) === "s") {
-        newEmojiString = emojiString.slice(
-          0,
-          newEmojiString.length - removeNumCharsFromBack
-        );
-        emojiString = newEmojiString;
-        console.log("corrected emojiString is : " + emojiString);
-      } 
-      else if (newEmojiString.charAt(newEmojiString.length - 1) === "p") {
-        newEmojiString = emojiString.slice(
-          0,
-          newEmojiString.length - removeNumCharsFromBack - 1
-        );
-        emojiString = newEmojiString;
-        console.log("corrected emojiString is : " + emojiString);
-      } else {
-        emojiString = newEmojiString;
-        checkString(emojiString);
-      }
-      newEmojiString = "";
-      console.log(`emojiString after BACKSPACE ${emojiString}`);
-      break;
-    case "KeyA":
-      emojiString += "🔓";
-      console.log("entered a");
-      break;
-    case "KeyB":
-      emojiString += "💪";
-      console.log("entered b");
-      break;
-    case "KeyC":
-      emojiString += "🗜️";
-      console.log("entered c");
-      break;
-    case "KeyD":
-      emojiString += "🍶";
-      console.log("entered d");
-      break;
-    case "KeyE":
-      emojiString += "🍥";
-      console.log("entered e");
-      break;
-    case "KeyF":
-      emojiString += "🌾";
-      console.log("entered f");
-      break;
-    case "KeyG":
-      emojiString += "🐬";
-      console.log("entered g");
-      break;
-    case "KeyH":
-      emojiString += "🐢";
-      console.log("entered h");
-      break;
-    case "KeyI":
-      emojiString += "📍";
-      console.log("entered i");
-      break;
-    case "KeyJ":
-      emojiString += "🏒";
-      console.log("entered j");
-      break;
-    case "KeyK":
-      emojiString += "🎿";
-      console.log("entered k");
-      break;
-    case "KeyL":
-      emojiString += "🕯️";
-      console.log("entered l");
-      break;
-    case "KeyM":
-      emojiString += "🎹";
-      console.log("entered m");
-      break;
-    case "KeyN":
-      emojiString += "⛩️";
-      console.log("entered n");
-      break;
-    case "KeyO":
-      emojiString += "📀";
-      console.log("entered o");
-      break;
-    case "KeyP":
-      emojiString += "🚩";
-      console.log("entered p");
-      break;
-    case "KeyQ":
-      emojiString += "🍭";
-      console.log("entered q");
-      break;
-    case "KeyR":
-      emojiString += "🥀";
-      console.log("entered r");
-      break;
-    case "KeyS":
-      emojiString += "⚡";
-      console.log("entered s");
-      break;
-    case "KeyT":
-      emojiString += "🍌";
-      console.log("entered t");
-      break;
-    case "KeyU":
-      emojiString += "👅";
-      console.log("entered u");
-      break;
-    case "KeyV":
-      emojiString += "💎";
-      console.log("entered v");
-      break;
-    case "KeyW":
-      emojiString += "🔱";
-      console.log("entered w");
-      break;
-    case "KeyX":
-      emojiString += "✂️";
-      console.log("entered x");
-      break;
-    case "KeyY":
-      emojiString += "🏆";
-      console.log("entered y");
-      break;
-    case "KeyZ":
-      emojiString += "💤";
-      console.log("entered z");
-      break;
-  }
-  console.log(emojiString);
-  emojifiedTxt.innerHTML = emojiString;
-  //console.log(emojifiedTxt);
-});
-*/
-
-
